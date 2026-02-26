6dd83cd7be25ea9d031a9dbd9caa6e898b930b46
parsaivi <parsa.malekian@divar.ir>
2026-02-26 12:57:28 +0330

    fix(backend): setting test added

---
+++ backend/test_settings.py
@@ -0,0 +1,21 @@
+"""
+Test settings for running tests with SQLite
+"""
+from config.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+# Disable migrations for faster tests
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+MIGRATION_MODULES = DisableMigrations()
